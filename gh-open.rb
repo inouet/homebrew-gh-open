4d35d69f34449bdbf59db4f2b1d88e9942487ce2
Taiji Inoue <inudog@gmail.com>
2021-05-09 16:50:14 +0900

    Fix sha

--- gh-open.rb
+++ gh-open.rb
@@ -5,7 +5,7 @@ class GhOpen < Formula
   version "v0.0.1"
 
   url "https://github.com/inouet/gh-open/releases/download/v0.0.1/gh-open_darwin_amd64.zip"
-  sha256 "56d8be557f54c381c36cdd0a180fd17b83f4129697a649f11af191ad32ed9ae8"
+  sha256 "3c7a3b20e42d96c0a376f801dc201719701d8ea1e0ab27cb0016a0c31575d394"
 
   def install
     bin.install "gh-open"
