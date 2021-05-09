33f42970e3aceeb8f06e29e8a13c9dd32cf1eae8
Taiji Inoue <inudog@gmail.com>
2021-05-09 16:30:37 +0900

    Update url/sha256

--- gh-open.rb
+++ gh-open.rb
@@ -4,8 +4,8 @@ class GhOpen < Formula
   homepage "https://github.com/inouet/gh-open"
   version "v0.0.1"
 
-  url "https://github.com/inouet/gh-open/releases/download/v0.0.1/gh-open_darwin_amd64"
-  sha256 "03253d49fb7033f6231c753fe2e445f4862c7b51fc686e0215f0b16a48098411"
+  url "https://github.com/inouet/gh-open/releases/download/v0.0.1/gh-open_darwin_amd64.zip"
+  sha256 "56d8be557f54c381c36cdd0a180fd17b83f4129697a649f11af191ad32ed9ae8"
 
   def install
     bin.install "gh-open"
