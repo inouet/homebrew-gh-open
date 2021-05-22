c2c5fe2f96f6ce724f548af0c066a1a5f66a96fc
Taiji Inoue <inudog@gmail.com>
2021-05-22 12:13:15 +0900

    Update version (0.0.2)

--- gh-open.rb
+++ gh-open.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class GhOpen < Formula
   homepage "https://github.com/inouet/gh-open"
-  version "v0.0.1"
+  version "v0.0.2"
 
-  url "https://github.com/inouet/gh-open/releases/download/v0.0.1/gh-open_darwin_amd64.zip"
-  sha256 "3c7a3b20e42d96c0a376f801dc201719701d8ea1e0ab27cb0016a0c31575d394"
+  url "https://github.com/inouet/gh-open/releases/download/v0.0.2/gh-open_darwin_amd64.zip"
+  sha256 "30a9d004d919d82452b086ccd2d48f35785dc07c7433dc3fbb1bccb129c1c51d"
 
   def install
     bin.install "gh-open_darwin_amd64" => "gh-open"
